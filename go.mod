d83d45819743ba55ec2972957a1d2ba62ce343c5
Sergey Batalev <sergey.batalev@gmail.com>
2020-08-06 22:56:17 +0300

    creating files from bin-data

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/likipiki/doc-gen
 
 go 1.14
 
-require github.com/spf13/cobra v1.0.0
+require (
+	github.com/pkg/errors v0.8.0
+	github.com/spf13/cobra v1.0.0
+)
--- internal/utility/main.go
+++ internal/utility/main.go
@@ -5,11 +5,14 @@ import (
 	"log"
 	"os"
 
+	"github.com/likipiki/doc-gen/internal/templates"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
 const (
-	imagesFolderName = "images"
+	imagesFolderName   = "images"
+	templateFolderName = "template"
 )
 
 var (
@@ -22,31 +25,91 @@ var (
 	}
 )
 
+func init() {
+	rootCmd.AddCommand(fullDocCmd)
+}
+
 // Execute executes the root command.
 func Execute() error {
 	return rootCmd.Execute()
 }
 
-func init() {
-	rootCmd.AddCommand(fullDocCmd)
-}
-
 func createFullDoc(cmd *cobra.Command, args []string) {
 	fmt.Println("Creating full doc...")
-	name, _ := os.Executable()
 
-	fmt.Println(name)
+	// scan template files and copy it to repo
+	files, err := templates.AssetDir("templates/full")
 
-	_, err := os.Stat(imagesFolderName)
+	if err != nil {
+		log.Println(
+			errors.Wrap(err, "cannot read asset directory"),
+		)
+		return
+	}
 
-	if os.IsNotExist(err) {
-		errDir := os.MkdirAll(imagesFolderName, 0755)
-		if errDir != nil {
-			log.Fatal(err)
+	fmt.Println("Copy files...")
+	for _, filename := range files {
+		data, err := templates.Asset("templates/full/" + filename)
+
+		if err != nil {
+			log.Println(errors.Wrap(err, "cannot read file bin data"))
 			return
 		}
-		fmt.Println("image folder created")
-	} else {
-		fmt.Println("image folder is exists")
+
+		err = writeToFile(filename, data)
+		if err != nil {
+			log.Println("Error, cannot write to file", err)
+		}
+
+		fmt.Printf("- %s\n", filename)
 	}
+
+	fmt.Println("Template full created!")
+}
+
+func writeToFile(filename string, content []byte) error {
+	file, err := os.Create(filename)
+
+	if err != nil {
+		return errors.Wrap(
+			err,
+			"can not create file - "+filename,
+		)
+	}
+	defer file.Close()
+
+	_, err = file.Write(content)
+	if err != nil {
+		return errors.Wrap(
+			err,
+			"unable write content to file - "+filename,
+		)
+	}
+
+	return nil
+}
+
+func createFoldersIfNeed(folders []string) error {
+	for _, folder := range folders {
+
+		// create folder if not exists
+		_, err := os.Stat(imagesFolderName)
+
+		if os.IsNotExist(err) {
+			errDir := os.MkdirAll(imagesFolderName, 0755)
+
+			if errDir != nil {
+				return errors.Wrap(
+					err,
+					"cannot make directory"+folder,
+				)
+			}
+
+			fmt.Println(folder + " folder created")
+		} else {
+			fmt.Println(folder + " folder is exists")
+		}
+	}
+
+	return nil
 }
